98d04f1a704e0bac74cc1c9ecec7ccd193913d35
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-07-09 16:26:10 -0500

    Update get_shinyrates_data.R

--- R/get_shinyrates_data.R
+++ R/get_shinyrates_data.R
@@ -24,7 +24,7 @@ get_shinyrates_data <- function(
 
   # get the date
   suppressMessages(
-    now <- timestamp(
+    now <- utils::timestamp(
     stamp = Sys.Date(),
     prefix = "",
     suffix = ""
